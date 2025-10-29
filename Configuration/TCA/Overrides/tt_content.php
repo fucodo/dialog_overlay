26d268b791d13071cd7b75a5d68de4f594c19bd7
Kay Strobach <info@kay-strobach.de>
2025-10-29 09:21:55 +0100

    [TASK] add tt_content fields
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -93,6 +93,16 @@ defined('TYPO3') || die();
             --palette--;;general, header;Titel,
             --div--;Einstellungen,
                 cookie_lifetime_days, cookie_name, auto_open, show_close_button, close_button_label,
+             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                --palette--;;frames,
+                --palette--;;appearanceLinks,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                --palette--;;language, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                --palette--;;hidden, --palette--;;access, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                categories,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                rowDescription,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended
     ';
 })();
 
